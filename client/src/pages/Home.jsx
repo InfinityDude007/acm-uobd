c9cfa1622db7d22f921dfe45c3b39d73bd9aa681
Armaan Jhanji <armaanj32@gmail.com>
2025-10-13 21:00:06 +0400

    refactor: temporarily remove non-existent components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,6 @@ import "../assets/css/Home/Home.css";
 import Hero from "../components/Home/Hero";
 import About from "../components/Home/About";
 import Testimonial from "../components/Home/Testimonial";
-// import Services from "../components/Services";
-// import Newsletter from "../components/Newsletter";
-// import CaseStudies from "../components/CaseStudies";
-// import Portfolio from "../components/Portfolio";
-// import Testimonials from "../components/Testimonials";
-// import FAQs from "../components/FAQs";
 
 const Home = () => {
     return (
@@ -16,11 +10,6 @@ const Home = () => {
             <Hero />
             <About />
             <Testimonial />
-            {/* <Services />
-            <Newsletter />
-            <CaseStudies />
-            <Portfolio />
-            <FAQs /> */}
         </div>
     );
 };
