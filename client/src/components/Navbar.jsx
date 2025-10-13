e945e0f07be74fccd74b699e2b2ff9ddbd22d355
Armaan Jhanji <armaanj32@gmail.com>
2025-10-13 20:43:26 +0400

    feat: add sticky navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import "../assets/css/Navbar.css";
+
+const Navbar = () => {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrolled(window.scrollY > 50);
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    return (
+        <div
+            className={`container-fluid navbar-wrapper ${
+                scrolled ? "navbar-scrolled" : "navbar-transparent"
+            } fixed-top`}
+        >
+            <div className="container">
+                <nav className="navbar navbar-expand-lg navbar-dark p-0">
+                    <Link to="/" className="navbar-brand">
+                        <h1 className="text-white">
+                            ACM<span className="text-dark">.</span>UoBD
+                        </h1>
+                    </Link>
+
+                    <button
+                        type="button"
+                        className="navbar-toggler ms-auto me-0"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarCollapse"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarCollapse">
+                        <div className="navbar-nav ms-auto">
+                            <Link to="/about" className="nav-item nav-link">
+                                About
+                            </Link>
+                            <Link to="/team" className="nav-item nav-link">
+                                Our Team
+                            </Link>
+                            <Link to="/events" className="nav-item nav-link">
+                                Events
+                            </Link>
+                            <Link to="/projects" className="nav-item nav-link">
+                                Projects
+                            </Link>
+                            <Link to="/research" className="nav-item nav-link">
+                                Research
+                            </Link>
+
+                            <div className="nav-item dropdown">
+                                <a
+                                    href="#"
+                                    className="nav-link dropdown-toggle"
+                                    data-bs-toggle="dropdown"
+                                >
+                                    More
+                                </a>
+                                <div className="dropdown-menu bg-light mt-2">
+                                    <Link to="/faq" className="dropdown-item">
+                                        FAQs
+                                    </Link>
+                                    <a
+                                        href="mailto:acmuobd@gmail.com"
+                                        className="dropdown-item"
+                                    >
+                                        Contact Us
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <button
+                            type="button"
+                            className="btn text-white p-0 d-none d-lg-block"
+                            data-bs-toggle="modal"
+                            data-bs-target="#searchModal"
+                        >
+                            <FontAwesomeIcon icon={faSearch} />
+                        </button>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
