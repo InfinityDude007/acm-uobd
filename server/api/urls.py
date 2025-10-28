cb18240fd300cf2e9d068a74186f6a55d552bebf
Armaan Jhanji <armaanj32@gmail.com>
2025-10-29 01:41:17 +0400

    fix: allow django to serve media outside of debug mode

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from django.conf import settings
-import os
 from django.conf.urls.static import static
+import os
 from . import views
+
 urlpatterns = [
     path('health/', views.health_check, name='health_check'),
     path('events/', views.events_view, name='events_view'),
     path('events/<int:event_id>/register/', views.register_for_event, name='register_for_event'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
