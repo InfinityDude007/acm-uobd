97ea34dc73b9c0c65598be364b7e2bfeec6255e0
Armaan Jhanji <armaanj32@gmail.com>
2025-11-10 15:29:19 +0400

    chore: remove unused imports

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,7 @@
-import React from "react";
 import Hero from "../components/Home/Hero";
 import About from "../components/Home/About";
 import Services from "../components/Home/Services";
 import Testimonial from "../components/Home/Testimonial";
-// import Services from "../components/Services";
-// import Newsletter from "../components/Newsletter";
-// import CaseStudies from "../components/CaseStudies";
 import Portfolio from "../components/Home/Portfolio";
 import FAQs from "../components/Home/Faq"
 
